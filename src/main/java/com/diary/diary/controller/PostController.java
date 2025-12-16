caf68599af528e3a8e4319279e53bdf86ce8551f
Kippzta <erik.rehnberg@hotmail.com>
2025-12-16 17:50:10 +0100

    added showPost controller and view

--- src/main/java/com/diary/diary/controller/PostController.java
+++ src/main/java/com/diary/diary/controller/PostController.java
@@ -4,8 +4,10 @@ import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.diary.diary.model.DiaryPost;
@@ -25,5 +27,12 @@ public class PostController {
         return "redirect:/home";
     }
 
-    
+    @GetMapping("/showPost/{postId}")
+    private String showPost(@PathVariable int postId, Model model)
+    {
+        DiaryPost post = diaryRepository.findById(postId).orElse(null);
+        model.addAttribute("post", post);
+        return "showPost";
+    }
+
 }
