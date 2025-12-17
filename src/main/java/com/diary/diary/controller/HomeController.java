bb7cb1b793caa7e0810db0471d71afb419b3d18b
Kippzta <erik.rehnberg@hotmail.com>
2025-12-17 17:28:06 +0100

    Added delete post function and some CSS

--- src/main/java/com/diary/diary/controller/HomeController.java
+++ src/main/java/com/diary/diary/controller/HomeController.java
@@ -1,13 +1,9 @@
 package com.diary.diary.controller;
 
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 import com.diary.diary.repositories.DiaryRepository;
 
